5605c3c1cdf26e8965acb3d35810ba72e2b3e767
Daniel Rosendo Vergara <dvergara@hargreaves.cs.arizona.edu>
2017-09-11 12:48:48 -0700

    Gold leader has commited his changes

--- Hello.java
+++ Hello.java
@@ -9,25 +9,25 @@ public class Hello {
     }
 
     public static void callSign() {
-        
-        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
-        system.out.println("Lock S-foils in attakc position.");
-        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
+        // Red Leader should add a print statement that says "Lock S-foils in attack position."
+        //System.out.println("Lock S-foils in attakc position.");
+        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
+        //System.out.println("Stabalize your rear deflectors... Watch for enemy fighters.");
     }
 
     public static void goldLeader() {
 
         // Gold Leader should add a print statement that says "Loosen up!"
-    
+        System.out.println("Loosen up!");
 
     }
 
     public static void redLeader() {
- 
+
         // Red Leader should add a print statement that says "I copy, gold leader!"
-        system.out.println("I copy, gold leader!");
-        
+        System.out.println("I copy, gold leader!");
+
 
     }
 
